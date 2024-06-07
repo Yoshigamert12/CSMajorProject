28b7de0b42234087ec08b0dfa1ce1e585ee8fb4f
Yoshigamert12 <159061386+Yoshigamert12@users.noreply.github.com>
2024-06-07 12:36:40 -0600

    Got to have the route movement

--- TextBasedAdvDemo/src/App.java
+++ TextBasedAdvDemo/src/App.java
@@ -3,13 +3,31 @@ import java.util.Scanner;
 
 public class App {
     private static String[] QTEXT; //Stores question text
+    private static int[][] QROUTE; //Next question
     private static int[][] QVAL; //Stores choice consequence/affect
     private static int[] userChoice; //Stores the user responses
     private static int numofQ = 10; //Number of questions
-    
+    private static int maxChoices = 3; //number of options for response
+    // Make the Counter
     public static void main(String[] args) throws Exception {
         gameSetup();
+        Scanner keys = new Scanner(System.in);
 
+        //Game loop
+        for (int i = 0; i < numofQ; i++) {
+            //Prompt the uswer with the question
+            System.out.println(QTEXT[i]);
+            int inp = keys.nextInt();
+            System.out.println("Your choice was " + inp + ". The affect is this " + QVAL[i][inp]);
+
+            //Do we need to skip?
+            if (QROUTE[i][inp] > -1) {
+                i = QROUTE[i][inp] - 1; //subtract to offset the loop increment
+            }
+
+        }
+
+        System.out.println("EXITED");
 
     }
 
@@ -20,9 +38,18 @@ public class App {
     public static void gameSetup() {
         //Creating the Arrays
         QTEXT = new String[numofQ];
-        QVAL = new int[numofQ][3]; //max 3 choices/options
+        QVAL = new int[numofQ][maxChoices]; //max 3 choices/options
+        QROUTE = new int[numofQ][maxChoices]; //MAx 3 options
         userChoice = new int[numofQ];
 
+        //Set all routes to -1 as default
+        for (int i = 0; i<numofQ; i++) {
+            for(int j = 0; j<maxChoices; j++) {
+                QROUTE[i][j] = -1;
+            }
+        }
+
+        //Question database
         for(int i = 0; i<numofQ; i++) {
             switch(i) {
                 //first Question
@@ -30,7 +57,10 @@ public class App {
                     QTEXT[i] = "Congragulations you woke up! how do you want to start your day?";
                     QTEXT[i] = QTEXT[i] + "\n type 0 for sleep in";
                     QTEXT[i] = QTEXT[i] + "\n type 1 get out of bed and get ready ";
+                    
                     QVAL[i][0] = -15; //Option 1 affect
+                    QROUTE[i][0] = 4;
+
                     QVAL[i][1] = 20; //Option 2 affect
                     QVAL[i][2] = 0; //Option 3 affect
                     
